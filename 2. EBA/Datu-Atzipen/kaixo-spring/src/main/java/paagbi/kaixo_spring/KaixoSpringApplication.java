ae5134b644af4c2b76ac0ddf6272490091955bd4
IkerEga <egana.iker@uni.eus>
2025-12-16 09:14:07 +0100

    spring endpoint erabilera

---
+++ 2. EBA/Datu-Atzipen/kaixo-spring/src/main/java/paagbi/kaixo_spring/KaixoSpringApplication.java
@@ -0,0 +1,43 @@
+package paagbi.kaixo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class KaixoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaixoSpringApplication.class, args);
+	}
+
+	@SpringBootApplication
+            @RestController
+            public class LehenrestapiaApplication {
+            
+                public static void main(String[] args) {
+                    SpringApplication.run(LehenrestapiaApplication.class, args);
+                }
+                @GetMapping("/")
+                public String home() {
+                    return "Gure lehen rest apia martxan dago."; 
+                }
+                
+                @GetMapping("/kaixo")
+                public String hello(@RequestParam(value = "izena", defaultValue = "mundua") String izena) {
+                    return String.format("Kaixo %s!", izena);
+                }
+            
+                @GetMapping("/zenbatu")
+                public String zenbatu(@RequestParam(value = "norarte", defaultValue = "5") String norarte) {
+            
+                    String strEmaitza = "";
+                    for (int i = 1; i <= Integer.parseInt(norarte); i++) {
+                        strEmaitza += (i + "\n");
+                    }
+                    return strEmaitza;
+                }
+			}
+}
