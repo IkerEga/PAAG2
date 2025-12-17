1b755b9d1018bedc5b17f77e44191a08e62241f9
IkerEga <egana.iker@uni.eus>
2025-12-17 13:06:47 +0100

    proiektu berria

---
+++ 2. EBA/Datu-Atzipen/05-rest-mongo-umeak/src/test/java/paagbi/__rest_mongo_umeak/ApplicationTests.java
@@ -0,0 +1,13 @@
+package paagbi.__rest_mongo_umeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
