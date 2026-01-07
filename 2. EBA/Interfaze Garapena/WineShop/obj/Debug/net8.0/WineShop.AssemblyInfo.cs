60cecf11f979174e04e3df09f25c4f078b8a37f1
IkerEga <egana.iker@uni.eus>
2026-01-07 10:21:29 +0100

    aldaketak

--- 2. EBA/Interfaze Garapena/WineShop/obj/Debug/net8.0/WineShop.AssemblyInfo.cs
+++ 2. EBA/Interfaze Garapena/WineShop/obj/Debug/net8.0/WineShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04bf51e2df8ddaa095119f2d21d8a0657fc96ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8667963336f51282ffc03b853d32e9dc9d29d034")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
